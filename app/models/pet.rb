a9dedff34813a01b8e227db08f2dd8b109939f32
YOUR_GITHUB_USERNAME <dave.sloan@gmail.com>
2015-12-15 14:35:37 -0800

    first commit with Pet model validations

--- app/models/pet.rb
+++ app/models/pet.rb
@@ -1,2 +1,6 @@
 class Pet < ActiveRecord::Base
+
+	validates :name, presence: true, uniqueness: true, length: { minimum: 3 }
+	validates :breed, presence: true, uniqueness: true, length: { minimum: 6 }
+
 end
