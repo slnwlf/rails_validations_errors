d2cf80f38a998222cda72d7e8b257db453482cbd
YOUR_GITHUB_USERNAME <dave.sloan@gmail.com>
2015-12-15 15:27:16 -0800

    got basic flash messages working

--- app/models/pet.rb
+++ app/models/pet.rb
@@ -1,6 +1,6 @@
 class Pet < ActiveRecord::Base
 
 	validates :name, presence: true, uniqueness: true, length: { minimum: 3 }
-	validates :breed, presence: true, uniqueness: true, length: { minimum: 6 }
+	validates :breed, presence: true
 
 end
